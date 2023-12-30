9e068992fd90347b886c6d12ca7783eee619d2ca
Digamber Jha <kumardigamberjha@gmail.com>
2023-12-30 17:38:22 +0530

    Working on chnages

--- redcar/settings.py
+++ redcar/settings.py
@@ -86,7 +86,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'redcar.wsgi.application'
 
-DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600 
+FILE_UPLOAD_MAX_MEMORY_SIZE = 124288000
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
