2e595004f80e1a90f9a2e138505f8626750d1dcd
Digamber Jha <kumardigamberjha@gmail.com>
2023-12-30 11:58:43 +0530

    Working on changes

--- red/views.py
+++ red/views.py
@@ -58,31 +58,8 @@ def Index(request):
         api_key = '427870336a6d45359a394f7f89cf5cba'
         mobile_number = '7814574759'  # Replace XXXX with the actual number
 
-        url = "http://web.cloudwhatsapp.com/wapp/api/send?apikey={}&mobile={}&msg={}&img1={}".format(
-            api_key, phone, f'''Welcome to RedCarpet!
-
-            Dear Guest,
-
-            We extend a warm welcome and present your exclusive access pass. To ensure a seamless experience, please follow these instructions:
-
-            To Check the current status of the pass:
-
-            https://redcarpet.codingindia.co.in/CheckQRCode/{phone}/
-
-            Pass Deactivation Instructions:
-
-            Upon entry, your access pass will be activated. To deactivate the pass after entry, please use the following deactivation link:
-
-            https://redcarpet.codingindia.co.in/HandleQRCode/{phone}/ 
-
-            Important Note:
-            Please refrain from clicking on the deactivation link before entering the venue to avoid premature deactivation of your pass.
-
-            Thank you for choosing RedCarpet. We look forward to hosting you and hope you enjoy your time with us!
-
-            Warm regards,
-
-            {request.user}''', f"https://redcarpet.codingindia.co.in/media/{s.img}"
+        url = "http://web.cloudwhatsapp.com/wapp/api/send?apikey={}&mobile={}&img1={}".format(
+            api_key, phone, f"https://redcarpet.codingindia.co.in/media/{s.img}"
         )
 
         try:
